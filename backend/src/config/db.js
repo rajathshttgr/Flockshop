5ef3d6d37482dda27e88f7a09b0afeb0fd3a3649
Rajath Shettigar <rajathshettigar.uy@gmail.com>
2025-05-05 18:14:33 +0530

    Intial backend commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,32 @@
+// db.js
 import pkg from "pg";
+const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
-const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-});
-
-pool.on("connect", () => {
-  console.log("Connected to the database");
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Neon
+  },
 });
 
 export default pool;
+
+// import pkg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// pool.on("connect", () => {
+//   console.log("Connected to the database");
+// });
+
+// export default pool;
